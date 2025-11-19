c62039d523481895a52b6cc57042701609339867
UnderscoreLodii <krzak.dan@gmail.com>
2025-11-19 20:03:50 +0100

    done2

--- src/main/java/uj/wmii/pwj/map2d/HashMap2D.java
+++ src/main/java/uj/wmii/pwj/map2d/HashMap2D.java
@@ -3,7 +3,6 @@ package uj.wmii.pwj.map2d;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Set;
 import java.util.function.Function;
 
 public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
@@ -18,16 +17,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         this.size = 0;
     }
 
-    /**
-     * Puts a value to the map, at given row and columns keys.
-     * If specified row-column key already contains element, it should be replaced.
-     *
-     * @param rowKey    row part of the key.
-     * @param columnKey column part of the key.
-     * @param value     object to put. It can be null.
-     * @return object, that was previously contained by map within these coordinates, or {@code null} if it was empty.
-     * @throws NullPointerException if rowKey or columnKey are {@code null}.
-     */
     @Override
     public V put(R rowKey, C columnKey, V value) throws NullPointerException {
         if (rowKey == null || columnKey == null) throw new NullPointerException();
@@ -39,13 +28,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return oldValue;
     }
 
-    /**
-     * Gets a value from the map from given key.
-     *
-     * @param rowKey    row part of a key.
-     * @param columnKey column part of a key.
-     * @return object contained at specified key, or {@code null}, if the key does not contain an object.
-     */
     @Override
     public V get(R rowKey, C columnKey) {
         Map<C, V> view = rowStorage.get(rowKey);
@@ -53,14 +35,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return view.get(columnKey);
     }
 
-    /**
-     * Gets a value from the map from given key. If specified value does not exist, returns {@code defaultValue}.
-     *
-     * @param rowKey       row part of a key.
-     * @param columnKey    column part of a key.
-     * @param defaultValue value to be returned, if specified key does not contain a value.
-     * @return object contained at specified key, or {@code defaultValue}, if the key does not contain an object.
-     */
     @Override
     public V getOrDefault(R rowKey, C columnKey, V defaultValue) {
         Map<C, V> view = rowStorage.get(rowKey);
@@ -68,13 +42,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return view.getOrDefault(columnKey, defaultValue);
     }
 
-    /**
-     * Removes a value from the map from given key.
-     *
-     * @param rowKey    row part of a key.
-     * @param columnKey column part of a key.
-     * @return object contained at specified key, or {@code null}, if the key didn't contain an object.
-     */
     @Override
     public V remove(R rowKey, C columnKey) {
         Map<C, V> row = rowStorage.get(rowKey);
@@ -89,39 +56,21 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return oldValue;
     }
 
-    /**
-     * Checks if map contains no elements.
-     *
-     * @return {@code true} if map doesn't contain any values; {@code false} otherwise.
-     */
     @Override
     public boolean isEmpty() {
         return size == 0;
     }
 
-    /**
-     * Checks if map contains any element.
-     *
-     * @return {@code true} if map contains at least one value; {@code false} otherwise.
-     */
     @Override
     public boolean nonEmpty() {
         return size > 0;
     }
 
-    /**
-     * Return number of values being stored by this map.
-     *
-     * @return number of values being stored
-     */
     @Override
     public int size() {
         return this.size;
     }
 
-    /**
-     * Removes all objects from a map.
-     */
     @Override
     public void clear() {
         size = 0;
@@ -129,27 +78,13 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         columnStorage.clear();
     }
 
-    /**
-     * Returns a view of mappings for specified key.
-     * Result map should be immutable. Later changes to this map should not affect result map.
-     *
-     * @param rowKey row key to get view map for.
-     * @return map with view of particular row. If there is no values associated with specified row, empty map is returned.
-     */
-    @Override
+   @Override
     public Map<C, V> rowView(R rowKey) {
         Map<C, V> view = rowStorage.get(rowKey);
         if (view == null) return Collections.emptyMap();
         return Map.copyOf(view);
     }
 
-    /**
-     * Returns a view of mappings for specified column.
-     * Result map should be immutable. Later changes to this map should not affect returned map.
-     *
-     * @param columnKey column key to get view map for.
-     * @return map with view of particular column. If there is no values associated with specified column, empty map is returned.
-     */
     @Override
     public Map<R, V> columnView(C columnKey) {
         Map<R, V> view = columnStorage.get(columnKey);
@@ -157,12 +92,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return Map.copyOf(view);
     }
 
-    /**
-     * Checks if map contains specified value.
-     *
-     * @param value value to be checked
-     * @return {@code true} if map contains specified value; {@code false} otherwise.
-     */
     @Override
     public boolean containsValue(V value) {
         for(Map<C, V> colMap : rowStorage.values()){
@@ -171,13 +100,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return false;
     }
 
-    /**
-     * Checks if map contains a value under specified key.
-     *
-     * @param rowKey    row key to be checked
-     * @param columnKey column key to be checked
-     * @return {@code true} if map contains specified key; {@code false} otherwise.
-     */
     @Override
     public boolean containsKey(R rowKey, C columnKey) {
         Map<C, V> row = rowStorage.get(rowKey);
@@ -185,34 +107,16 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return row.containsKey(columnKey);
     }
 
-    /**
-     * Checks if map contains at least one value within specified row.
-     *
-     * @param rowKey row to be checked
-     * @return {@code true} if map at least one value within specified row; {@code false} otherwise.
-     */
     @Override
     public boolean containsRow(R rowKey) {
         return rowStorage.containsKey(rowKey);
     }
 
-    /**
-     * Checks if map contains at least one value within specified column.
-     *
-     * @param columnKey column to be checked
-     * @return {@code true} if map at least one value within specified column; {@code false} otherwise.
-     */
     @Override
     public boolean containsColumn(C columnKey) {
         return columnStorage.containsKey(columnKey);
     }
 
-    /**
-     * Return a view of this map as map of rows to map of columns to values.
-     * Result map should be immutable. Later changes to this map should not affect returned map.
-     *
-     * @return map with row-based view of this map. If this map is empty, empty map should be returned.
-     */
     @Override
     public Map<R, Map<C, V>> rowMapView() {
         if(isEmpty()) return Collections.emptyMap();
@@ -223,12 +127,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return Collections.unmodifiableMap(deepCopy);
     }
 
-    /**
-     * Return a view of this map as map of columns to map of rows to values.
-     * Result map should be immutable. Later changes to this map should not affect returned map.
-     *
-     * @return map with column-based view of this map. If this map is empty, empty map should be returned.
-     */
     @Override
     public Map<C, Map<R, V>> columnMapView() {
         if(isEmpty()) return Collections.emptyMap();
@@ -239,13 +137,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return Collections.unmodifiableMap(deepCopy);
     }
 
-    /**
-     * Fills target map with all key-value maps from specified row.
-     *
-     * @param target map to be filled
-     * @param rowKey row key to get data to fill map from
-     * @return this map (floating)
-     */
     @Override
     public Map2D<R, C, V> fillMapFromRow(Map<? super C, ? super V> target, R rowKey) {
         Map <C, V> row =  rowStorage.get(rowKey);
@@ -253,13 +144,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return this;
     }
 
-    /**
-     * Fills target map with all key-value maps from specified row.
-     *
-     * @param target    map to be filled
-     * @param columnKey column key to get data to fill map from
-     * @return this map (floating)
-     */
     @Override
     public Map2D<R, C, V> fillMapFromColumn(Map<? super R, ? super V> target, C columnKey) {
         Map <R, V> column =  columnStorage.get(columnKey);
@@ -267,13 +151,7 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return this;
     }
 
-    /**
-     * Puts all content of {@code source} map to this map.
-     *
-     * @param source map to make a copy from
-     * @return this map (floating)
-     */
-    @Override
+   @Override
     public Map2D<R, C, V> putAll(Map2D<? extends R, ? extends C, ? extends V> source) {
         for(var rowEntry : source.rowMapView().entrySet()) {
             for (var columnEntry : rowEntry.getValue().entrySet()) {
@@ -283,14 +161,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return this;
     }
 
-    /**
-     * Puts all content of {@code source} map to this map under specified row.
-     * Ech key of {@code source} map becomes a column part of key.
-     *
-     * @param source map to make a copy from
-     * @param rowKey object to use as row key
-     * @return this map (floating)
-     */
     @Override
     public Map2D<R, C, V> putAllToRow(Map<? extends C, ? extends V> source, R rowKey) {
         for (var entry : source.entrySet()) {
@@ -299,14 +169,6 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return this;
     }
 
-    /**
-     * Puts all content of {@code source} map to this map under specified column.
-     * Ech key of {@code source} map becomes a row part of key.
-     *
-     * @param source    map to make a copy from
-     * @param columnKey object to use as column key
-     * @return this map (floating)
-     */
     @Override
     public Map2D<R, C, V> putAllToColumn(Map<? extends R, ? extends V> source, C columnKey) {
         for (var entry : source.entrySet()) {
@@ -315,17 +177,7 @@ public class HashMap2D<R, C, V> implements Map2D<R, C, V> {
         return this;
     }
 
-    /**
-     * Creates a copy of this map, with application of conversions for rows, columns and values to specified types.
-     * If as result of row or column conversion result key duplicates, then appropriate row and / or column in target map has to be merged.
-     * If merge ends up in key duplication, then it's up to specific implementation which value from possible to choose.
-     *
-     * @param rowFunction    function converting row part of key
-     * @param columnFunction function converting column part of key
-     * @param valueFunction  function converting value
-     * @return new instance of {@code Map2D} with converted objects
-     */
-    @Override
+   @Override
     public <R2, C2, V2> Map2D<R2, C2, V2> copyWithConversion(Function<? super R, ? extends R2> rowFunction, Function<? super C, ? extends C2> columnFunction, Function<? super V, ? extends V2> valueFunction) {
         Map2D<R2, C2, V2> newMap = new HashMap2D<>();
         for(var rowEntry : rowStorage.entrySet()) {
